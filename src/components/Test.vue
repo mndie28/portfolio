659b71a6c52117bb7677c4af31cc4402290f79b0
Amanda Chiabai <amandachiabai@Amandas-Mac-mini.local>
2018-08-21 14:15:52 -0400

    remove script
    removing grid script from test

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -51,15 +51,6 @@ export default {
 }
 </script>
 
-<script>
-  export default {
-    name: 'Grid',
-    data: () => ({
-      lorem: `Lorem ipsum dolor sit amet, mel at clita quando. Te sit oratio vituperatoribus, nam ad ipsum posidonium mediocritatem, explicari dissentiunt cu mea. Repudiare disputationi vim in, mollis iriure nec cu, alienum argumentum ius ad. Pri eu justo aeque torquatos.`
-    })
-  }
-</script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1, h2 {
